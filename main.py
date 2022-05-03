184f04322ffd6c7b6adce6e67248cceddbd4141c
skalpasi <40707813+skalpasi@users.noreply.github.com>
2022-05-03 22:41:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import PIL.Image
 import sys
-img = PIL.Image.open(sys.argv[1])
-img1 = img.load()
+
+f = PIL.Image.open(sys.argv[1])
+img = f.load()
+
 charset = [' ','.','°','*','o','O','#','@']
-for y in range(img.size[1]):
-    for x in range(img.size[0]):
-        print(charset[int(((img1[x,y][0]+img1[x,y][1]+img1[x,y][2]))/768*len(charset))],end=" ")
+for width in range(f.size[1]):
+    for height in range(f.size[0]):
+        r = img[height, width][0]
+        g = img[height, width][1]
+        b = img[height, width][2]
+        shade = (r + g + b)/3  # brightness of pixel
+        percShade = shade/256  # percentage of brightess (1 for maximum brightness)
+        charIndex = int(percShade * len(charset))  # pick character from charset according to the % brightness
+        # 100% brightness (percShade = 1) will pick the last element of charset which has the highest brightness
+        print(charset[charIndex], end=" ")
     print()
